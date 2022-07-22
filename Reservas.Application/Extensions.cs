8d5c04b81c68d1838d9b3153d062bd7ffffa657e
gustavito12 <gusavt1989@gmail.com>
2022-07-22 16:20:43 -0400

    Prueba 1 de Precommit

--- Reservas.Application/Extensions.cs
+++ Reservas.Application/Extensions.cs
@@ -9,13 +9,10 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Reservas.Application
-{
-    public static class Extensions
-    {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
-        {
-            services.AddMediatR(Assembly.GetExecutingAssembly());
+namespace Reservas.Application {
+    public static class Extensions {
+        public static IServiceCollection AddApplication(this IServiceCollection services) {
+            services.AddMediatR(Assembly.GetExecutingAssembly()
             services.AddTransient<IReservaService, ReservaService>();
             services.AddTransient<IReservaFactory, ReservaFactory>();
 
